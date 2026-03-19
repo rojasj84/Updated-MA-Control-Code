d6123adcef55d021befb4d884a84eb41c2b8b0f0
jyang-collab <jyang@carnegiescience.edu>
2026-03-19 12:13:07 -0400

    test

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
-Output
+ontrol_time = 0.0
+        self.press_prev_error = None
+        
+        self.current_target_pressure = None
+        self.current_target_temp = None
+        self.current_target_power = None
+        
+        self.current_press_time_remaining = None
+        self.current_temp_time_remaining = None
+        self.current_power_time_remain
